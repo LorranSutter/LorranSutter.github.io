253849300d6d988dd869c0d59f1b87c7dc7dba7f
Lorran Sutter <sutterlorran@gmail.com>
2020-07-17 01:03:32 -0400

    Include skills | experience | education

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
 function toggleDarkMode() {
     const body = document.getElementById("body");
-    const darkModeButton = document.getElementById("dark-mode-button");
+    // const darkModeButton = document.getElementById("dark-mode-button");
+    const buttons = document.querySelectorAll('button');
     const linkedinIcon = document.getElementById("linkedin-icon");
     const githubIcon = document.getElementById("github-icon");
 
     if (body.classList.contains("bg-dark")) {
         body.classList.remove("bg-dark");
-        darkModeButton.classList.remove("bg-dark");
-        darkModeButton.classList.add("bg-secondary");
         linkedinIcon.src = "img/linkedin-icon-dark.svg";
         githubIcon.src = "img/github-icon-dark.svg";
+
+        buttons.forEach(button => {
+            button.classList.remove('bg-dark');
+            button.classList.add('bg-secondary');
+        });
+
     } else {
         body.classList.add("bg-dark");
-        darkModeButton.classList.remove("bg-secondary");
-        darkModeButton.classList.add("bg-dark");
         linkedinIcon.src = "img/linkedin-icon-white.svg";
         githubIcon.src = "img/github-icon-white.svg";
+
+        buttons.forEach(button => {
+            button.classList.remove('bg-secondary');
+            button.classList.add('bg-dark');
+        });
     }
 }
 
