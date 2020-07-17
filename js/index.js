806f997d5f28e981b71d853089582dd61134db74
Lorran Sutter <sutterlorran@gmail.com>
2020-07-17 16:45:22 -0400

    Minimal presentable

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function toggleDarkMode() {
     const body = document.getElementById("body");
-    // const darkModeButton = document.getElementById("dark-mode-button");
+    const darkModeButton = document.getElementById("dark-mode-button");
     const buttons = document.querySelectorAll('button');
     const linkedinIcon = document.getElementById("linkedin-icon");
     const githubIcon = document.getElementById("github-icon");
@@ -9,6 +9,7 @@ function toggleDarkMode() {
         body.classList.remove("bg-dark");
         linkedinIcon.src = "img/linkedin-icon-dark.svg";
         githubIcon.src = "img/github-icon-dark.svg";
+        darkModeButton.innerHTML = "Dark mode";
 
         buttons.forEach(button => {
             button.classList.remove('bg-dark');
@@ -19,6 +20,7 @@ function toggleDarkMode() {
         body.classList.add("bg-dark");
         linkedinIcon.src = "img/linkedin-icon-white.svg";
         githubIcon.src = "img/github-icon-white.svg";
+        darkModeButton.innerHTML = "Light mode";
 
         buttons.forEach(button => {
             button.classList.remove('bg-secondary');
